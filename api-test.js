492846cbab1f388dbe63bb88e6978c3d35b2c2eb
darkartaudio <mikey@mikeyallred.com>
2023-07-18 00:10:31 -0500

    chore: renamed app in package.json

--- api-test.js
+++ api-test.js
@@ -1,8 +1,5 @@
 const axios = require('axios');
 require('dotenv').config();
-const { firstLettersCapitalized } = require('./utils');
 
-// axios.get(`${process.env.API}/search.php?s=151`)
-// .then(result => console.log(result.data));
-
-console.log(firstLettersCapitalized('malt liquor'));
+axios.get(`${process.env.API}/search.php?s=151`)
+.then(result => console.log(result.data));
