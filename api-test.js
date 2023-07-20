7634375e0396b40be08a378df3cf167a12af0e71
Mikey Allred <122388609+darkartaudio@users.noreply.github.com>
2023-07-19 20:57:16 -0500

    Revert "Revert "Cleanup""

--- api-test.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-require('dotenv').config();
-
-axios.get(`${process.env.API}/search.php?s=151`)
-.then(result => console.log(result.data));
--- controllers/comments.js
+++ controllers/comments.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const { JWT_SECRET } = process.env;
-const { Comment, User, Recipe } = require('../models')
-const { parseValue } = require('../utils');
+const { Comment, User, Recipe } = require('../models');
 
 router.get('/', (req, res) => {
     Comment.find({})
@@ -14,69 +10,61 @@ router.get('/', (req, res) => {
     })
     .catch(error => {
         console.log('error', error);
-        res.json({ message: 'There was an issue please try again...'})
-    })
-})
+        return res.json({ message: 'There was an issue please try again...'});
+    });
+});
 
 router.get('/:field/:value', (req, res) => {
-    let field = req.params.field
-    let value = req.params.value
-    // console.log('field', 'value', field, value)
-    // let query = {}
-    // query[field]=value
+    let field = req.params.field;
+    let value = req.params.value;
     
     Comment.find({ [field]:[value] })
     .then((comments) => {
-        // console.log("comments", comments)
-        return res.json({ comments: comments })
+        return res.json({ comments: comments });
     })
     .catch(error => {
         console.log('error', error);
-        res.json({ message: 'There was an issue please try again...' });
+        return res.json({ message: 'There was an issue please try again...' });
     });
-})
+});
 
 router.get('/:id', (req, res) => {
     Comment.findById(req.params.id)
     .then((comment) => {
-        // console.log('comment found')
-        return res.json({ comment: comment})
+        return res.json({ comment: comment });
     })
     .catch(error => {
         console.log('error', error);
-        res.json({ message: 'There was an issue please try again...' });
+        return res.json({ message: 'There was an issue please try again...' });
     });
-})
+});
 
 router.post('/search', (req, res) => {
     Comment.find({ _id: { $in: req.body.commentIds } })
     .populate('createdBy')
     .then(comments => {
-        // console.log(comments);
         return res.json({ comments });
     })
     .catch(error => {
         console.log('error', error);
-        res.json({ message: 'There was an issue please try again...' });
+        return res.json({ message: 'There was an issue please try again...' });
     });
-})
+});
 
 router.post('/new', (req, res) => {
-    // console.log('data from request(comment)', req.body);
     Comment.create({
         title: req.body.title,
         body: req.body.body,
-        createdBy: req.body.createdBy,
+        createdBy: req.body.createdBy
     })
     .then((newComment) => {
-        // console.log('new comment created =>', newComment);
         return res.json({ comment: newComment });
     })
     .catch((error) => {
         console.log('error', error);
-        return res.json({ message: 'error occured, please try again.' });
+        res.json({ message: 'There was an issue please try again...' });
     });
-})
+});
 
 router.put('/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
     let comment = await Comment.findById(req.params.id).populate('createdBy');
@@ -85,7 +73,6 @@ router.put('/:id', passport.authenticate('jwt', { session: false }), async (req,
     }
 
     const updateQuery = {};
-    // check body
     if (req.body.body) {
         updateQuery.body = req.body.body;
     }
@@ -96,7 +83,7 @@ router.put('/:id', passport.authenticate('jwt', { session: false }), async (req,
         })
         .catch((error) => {
             console.log('error inside PUT /ingredients/:id', error);
-            return res.json({ message: 'error occured, please try again.' });
+            return res.json({ message: 'There was an issue please try again...'});
         });
 });
 
@@ -132,7 +119,7 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), async (r
         }
     } catch (error) {
         console.log('error inside DELETE /comments/:id', error);
-        return res.json({ message: 'error occured, please try again.' });
+        return res.json({ message: 'There was an issue please try again...'});
     }
 });
 
